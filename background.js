01572a7c8796d775e931580e8bd184451783539a
Nicholas Kolba <nicholaskolba@Nicholass-MacBook-Pro.local>
2019-12-03 02:53:14 -0500

    added basic intent resolution and automated intent handling for web apps

--- background.js
+++ background.js
@@ -16,7 +16,32 @@ let contextListeners = [];
 //intent listeners (dictionary keyed by intent name)
 let intentListeners = {};
 
-fetch("http://localhost:5556/directory.json").then(_r =>{
+/*var generateTemplateFunction = (function(){
+    var cache = {};
+
+    function generateTemplate(template){
+        var fn = cache[template];
+
+        if (!fn){
+            // Replace ${expressions} (etc) with ${map.expressions}.
+
+            var sanitized = template
+                .replace(/\$\{([\s]*[^;\s\{]+[\s]*)\}/g, function(_, match){
+                    return `\$\{map.${match.trim()}\}`;
+                    })
+                // Afterwards, replace anything that's not ${map.expressions}' (etc) with a blank string.
+                .replace(/(\$\{(?!map\.)[^}]+\})/g, '');
+
+            fn = Function('map', `return \`${sanitized}\``);
+        }
+
+        return fn;
+    }
+
+    return generateTemplate;
+})();*/
+
+fetch("http://localhost:3000/directory.json").then(_r =>{
                 let r = _r.clone();
                 r.json().then(data => {
                     directory = data;
@@ -25,8 +50,33 @@ fetch("http://localhost:5556/directory.json").then(_r =>{
 
 //to do: handle disconnects, remove listeners (identify listeners)
 chrome.runtime.onConnect.addListener(function(port) {
-    console.assert(port.name == "fdc3");
     connected[port.sender.url] = port;
+    let app_url = new URL(port.sender.url);
+    //look up in directory...
+    //to do: disambiguate apps with matching hosts
+    let entry = directory.find(ent => {
+        if (ent.start_url){
+            let ent_url = new URL(ent.start_url);
+            return app_url.host === ent_url.host;
+        }
+        return false;
+    });
+    //fetch and bundle the manifest data
+    if (entry){
+        if (entry.manifest){
+            fetch(entry.manifest).then(mR => {
+                mR.json().then(mD => {
+                    entry.manifestContent = mD;
+                    port.directoryData = entry;
+                    port.postMessage({name:"directoryData", data:port.directoryData});
+                });
+            });
+        }
+        else {
+            port.directoryData = entry;
+            port.postMessage({name:"directoryData", data:port.directoryData});
+        }
+    }
     port.onDisconnect.addListener(function(){
         console.log("disconnect",port);
         let id = port.sender.url;
@@ -41,13 +91,14 @@ chrome.runtime.onConnect.addListener(function(port) {
         });
     });
     port.onMessage.addListener(function(msg) {
+       
         if (msg.method === "open"){
             let result = directory.filter(item => item.name === msg.data.name);
             if (result.length > 0){
                 //get the manifest...
                 fetch(result[0].manifest).then(mR => {
                     mR.json().then(mD => {
-                        let win = window.open(mD.startup_app.url,msg.data.name);
+                        let win = window.open(mD.start_url,msg.data.name);
                         win.focus();
                         
                     });
@@ -73,10 +124,137 @@ chrome.runtime.onConnect.addListener(function(port) {
             });
         }
         else if (msg.method === "raiseIntent"){
+            let r = [];
+             //add dynamic listeners...
              if (intentListeners[msg.data.intent]) {
-                intentListeners[msg.data.intent][0].postMessage({name:"intent", data:msg.data});
+                intentListeners[msg.data.intent].forEach(win => {
+                    r.push({type:"window",details:win});
+                });
              }
+            //pull intent handlers from the directory
+            //to do: process directory when loaded (and refreshed...)
+            directory.forEach(entry => {
+                if (entry.intents){
+                    
+                    if (entry.intents.includes(msg.data.intent)){
+                        //ignore entries already dynamically registered
+                        let list = intentListeners[msg.data.intent];
+                        if (!list || !(list.find(app => {return app.directoryData.name === entry.name;}))){
+                            r.push({type:"directory", details:entry});
+                        }
+                    }
+                }
+            });
            
+            
+             if (r.length > 0){
+                if (r.length === 1){
+                    //if there is only one result, use that
+                 //if it is a window, post a message directly to it
+                 //if it is a directory entry resolve the destination for the intent and launch it
+                 //dedupe window and directory items
+                 if (r[0].type === "window"){
+                        r[0].details.postMessage({name:"intent", data:msg.data});
+                    } else if (r[0].type === "directory"){
+                        console.log("directory" + r[0].details);
+                        fetch(r[0].details.manifest).then(mR => {
+                            mR.json().then(mD => {
+                                //find the matching intent entry
+                                let intentData = mD.intents.find(i => {return i.intent === msg.data.intent;});
+                                //set paramters
+                                let ctx = msg.data.context;
+                                let params = {};
+                                Object.keys(contentManifest.params).forEach(key =>{ 
+                                    let param = contentManifest.params[key];
+                                    if (ctx.type === param.type){
+                                        if (param.key){
+                                            params[key] = ctx[param.key];
+                                        }
+                                        else if (param.id){
+                                            params[key]  = ctx.id[param.id]; 
+                                        }
+                                    }
+                                });
+                                //eval the url
+                                let template = mD.templates[intentData.template];
+                                Object.keys(params).forEach(key => {
+                                    template = template.replace("${" + key +"}",params[key]);
+
+                                });
+                               
+                                let start_url = template;
+                                let win = window.open(start_url,msg.data.name);
+                                win.focus();
+                                
+                            });
+                        });
+                    }
+                }
+                else {
+                    //show resolver UI
+                    // Send a message to the active tab
+                    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                        var activeTab = tabs[0];
+                        chrome.tabs.sendMessage(activeTab.id, {
+                            "message": "intent_resolver", 
+                            "data":r, 
+                            "intent":msg.data.intent,
+                            "context":msg.data.context});
+                    });
+                }
+                
+            }
+            else {
+                //show message indicating no handler for the intent...
+            }
+           
+        }
+        else if (msg.method === "resolveIntent"){
+            //find the app to route to
+            if (msg.selected.type === "window"){
+                let winList = intentListeners[msg.intent] ? intentListeners[msg.intent] : [];
+                let win = winList.find(item => {
+                    return item.sender && item.sender.id === msg.selected.details.sender.id;
+                });
+                if (win){
+                    win.postMessage({name:"intent", data:{intent:msg.intent, context: msg.context}});    
+                    win.focus();
+                }
+                
+            }
+            else if (msg.selected.type === "directory"){
+                fetch(msg.selected.details.manifest).then(mR => {
+                    mR.json().then(mD => {
+                  
+                        //find the matching intent entry
+                        let intentData = mD.intents.find(i => {return i.intent === msg.intent;});
+                        //set paramters
+                        let ctx = msg.context;
+                        let params = {};
+                        Object.keys(mD.params).forEach(key =>{ 
+                            let param = mD.params[key];
+                            if (ctx.type === param.type){
+                                if (param.key){
+                                    params[key] = ctx[param.key];
+                                }
+                                else if (param.id){
+                                    params[key]  = ctx.id[param.id]; 
+                                }
+                            }
+                        });
+                        //generate the url
+                        let template = mD.templates[intentData.template];
+                        Object.keys(params).forEach(key => {
+                            template = template.replace("${" + key +"}",params[key]);
+
+                        });
+                       
+                        let start_url = template;
+                        let win = window.open(start_url,msg.selected.details.name);
+                        win.focus();
+                    });
+                });
+            }
         }
     });
 });
--- content.js
+++ content.js
@@ -1,8 +1,14 @@
  //establish comms with the background script 
- var port = chrome.runtime.connect({name: "fdc3"});
+ let port = chrome.runtime.connect({name: "fdc3"});
+
+ //automated handlers based on manifest metadata - other handlers are set and dispatched by the API layer
+ //these just need to be markers - since the handling itself is just automgenerated from the metadata held in the manifest 
+ let _intentHandlers = [];
+ let _contextHandlers = [];
+ let contentManifest = null;
 
  //inject the FDC3 API
- var s = document.createElement('script');
+ let s = document.createElement('script');
  s.src = chrome.extension.getURL('api.js');
  s.onload = function() {
      this.parentNode.removeChild(this);
@@ -32,12 +38,55 @@ document.addEventListener('FDC3:addIntentListener',e => {
 });
 
 port.onMessage.addListener(msg => {
-    if (msg.name === "context"){
+    if (msg.name === "directoryData"){
+        console.log(msg.data);
+        //if there is manifest content, wire up listeners if intents and context metadata are there
+        let mani = msg.data.manifestContent;
+        //set global
+        contentManifest = mani;
+        if (mani && mani.intents){
+            //iterate through the intents, and set listeners
+            mani.intents.forEach(intent => {
+                port.postMessage({method:"addIntentListener", "data": {intent:intent.intent }}); 
+                _intentHandlers.push(intent.intent);
+            });
+          
+        }
+    }
+   else  if (msg.name === "context"){
         document.dispatchEvent(new CustomEvent("FDC3:context",{
             detail:{data:msg.data}
         }));
     }
     else if (msg.name === "intent") {
+        //check for handlers at the content script layer (automatic handlers) - if not, dispatch to the API layer...
+        if (_intentHandlers.indexOf(msg.data.intent) > -1 && contentManifest){
+            let intentData = contentManifest.intents.find(i => {return i.intent === msg.data.intent;});
+            //set paramters
+            let ctx = msg.data.context;
+            let params = {};
+           
+                Object.keys(contentManifest.params).forEach(key =>{ 
+                    let param = contentManifest.params[key];
+                    if (ctx.type === param.type){
+                        if (param.key){
+                            params[key] = ctx[param.key];
+                        }
+                        else if (param.id){
+                            params[key]  = ctx.id[param.id]; 
+                        }
+                    }
+                });
+            
+            //eval the url
+            let template = contentManifest.templates[intentData.template];
+            Object.keys(params).forEach(key => {
+                template = template.replace("${" + key +"}",params[key]);
+
+            });
+            window.location.href = template; 
+            window.focus();
+        }
         document.dispatchEvent(new CustomEvent("FDC3:intent",{
             detail:{data:msg.data}
         })); 
@@ -45,9 +94,6 @@ port.onMessage.addListener(msg => {
 
 });
 
-window.fdc3_2 = {
-    foo:"bar"
-};
  //handle intents
 
  //handle context
@@ -85,6 +131,52 @@ window.fdc3_2 = {
           }
         overlay.style.display = "block";
    
+      }
+      else if (request.message === "intent_resolver"){
+        if (! overlay){
+            overlay = document.createElement("div");
+            overlay.style.width = "400px";
+            overlay.style.height = "400px";
+            overlay.style.marginLeft = "-200px";
+            overlay.style.marginTop = "-200px";
+            overlay.style.left = "50%";
+           
+            overlay.style.top = "50%";
+            overlay.style.backgroundColor = "black";
+            overlay.style.position = "absolute";
+            overlay.style.display = "none;"
+            overlay.style.flexFlow = "row wrap";
+     document.body.appendChild(overlay);
+          }
+        overlay.style.display = "block";
+
+        let contents = `<div>Resolve Intent</div>
+        <div  id="fdc3-resolver-list" syle="height:300;overflow:scroll;">
+        </div>`;
+          overlay.innerHTML = contents;
+        //contents
+        request.data.forEach((item) => {
+            let selected = item;
+            let data = item.details.directoryData ? item.details.directoryData : item.details;
+            let rItem = document.createElement("div");
+            console.log(data);
+            rItem.style.color = "white";
+            rItem.style.flexFlow = "row";
+            rItem.style.height = "20px";
+            rItem.innerText = data.title
+            rItem.addEventListener("click",evt => {
+                //send resolution message to extension to route
+                port.postMessage({
+                    method:"resolveIntent",
+                    intent:request.intent,
+                    selected:selected,
+                    context:request.context
+                }); 
+                overlay.innerHTML = "";
+                overlay.style.display = "none";
+            });
+            overlay.appendChild(rItem);
+        });
       }
     }
   );
